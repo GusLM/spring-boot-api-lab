b2bcf3c554ad71fb3896109138f954512cf8e539
Gustavo Santos <gustavolimarma@gmail.com>
2026-03-19 23:13:27 -0300

    Add repository interfaces for Author, Book, Category, and Publisher entities

---
+++ building-api-training/rest-api-training/books_api/src/main/java/com/learning/books_api/repositories/BookRepository.java
@@ -0,0 +1,9 @@
+package com.learning.books_api.repositories;
+
+import com.learning.books_api.entities.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
