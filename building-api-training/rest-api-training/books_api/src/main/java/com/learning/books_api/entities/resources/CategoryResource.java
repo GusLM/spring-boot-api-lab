2f4af8de64b8ac1b419852494f13277c87acbdab
Gustavo Santos <gustavolimarma@gmail.com>
2026-03-20 00:49:47 -0300

    Add REST controller for Category with CRUD operations and pagination support

--- building-api-training/rest-api-training/books_api/src/main/java/com/learning/books_api/entities/resources/CategoryResource.java
+++ building-api-training/rest-api-training/books_api/src/main/java/com/learning/books_api/entities/resources/CategoryResource.java
@@ -1,4 +1,50 @@
 package com.learning.books_api.entities.resources;
 
+import com.learning.books_api.dto.PageResponse;
+import com.learning.books_api.entities.Category;
+import com.learning.books_api.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/categories")
 public class CategoryResource {
+
+    @Autowired
+    private CategoryService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Category> findById(@PathVariable Long id) {
+        Category category = service.findById(id);
+        return ResponseEntity.ok().body(category);
+    }
+
+    @GetMapping
+    public ResponseEntity<PageResponse<Category>> findAll(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        Page<Category> result = service.findAll(page,size);
+        return ResponseEntity.ok().body(PageResponse.from(result));
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> insert(@RequestBody Category obj) {
+        obj = service.insert(obj);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Category> update(@PathVariable Long id, Category obj) {
+        service.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
