553cf531bc639276ab7439d90f3534a24a8a167b
Gustavo Santos <gustavolimarma@gmail.com>
2026-04-17 01:51:12 -0300

    Add `StockMovement` entity and `StockMovementType` enum for tracking stock changes

--- building-api-training/rest-api-training/ecommerce-api/src/main/java/com/gustavosantos/ecommerce_api/model/StockMovement.java
+++ building-api-training/rest-api-training/ecommerce-api/src/main/java/com/gustavosantos/ecommerce_api/model/StockMovement.java
@@ -1,10 +1,55 @@
 package com.gustavosantos.ecommerce_api.model;
 
-import java.util.Date;
+import com.gustavosantos.ecommerce_api.model.enums.StockMovementType;
+import jakarta.persistence.*;
+import lombok.Getter;
 
+import java.util.Date;
+import java.util.Objects;
+
+@Getter
+@Entity
+@Table(name = "stock_movements")
 public class StockMovement {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "stock_product_id", nullable = false)
     private ProductStock productStock;
+
+    @ManyToOne
+    @JoinColumn(name = "order_item_id", nullable = false)
+    private OrderItem orderItem;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "movement_type", nullable = false, length = 20)
+    private StockMovementType movementType;
+
+    @Column(name = "created_at", nullable = false)
     private Date createdAt;
+
+    public StockMovement() {
+    }
+
+    public StockMovement(ProductStock productStock, OrderItem orderItem, StockMovementType movementType) {
+        this.productStock = productStock;
+        this.orderItem = orderItem;
+        this.movementType = movementType;
+        this.createdAt = new Date();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        StockMovement that = (StockMovement) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
---
+++ building-api-training/rest-api-training/ecommerce-api/src/main/java/com/gustavosantos/ecommerce_api/model/enums/StockMovementType.java
@@ -0,0 +1,6 @@
+package com.gustavosantos.ecommerce_api.model.enums;
+
+public enum StockMovementType {
+    IN,
+    OUT
+}
