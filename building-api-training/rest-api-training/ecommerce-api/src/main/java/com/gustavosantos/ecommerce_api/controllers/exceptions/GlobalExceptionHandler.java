788ca6411978b3bcb8a38ed39ff228b0a385fb6f
Gustavo Santos <gustavolimarma@gmail.com>
2026-04-28 01:25:06 -0300

    Add validation annotations to `CustomerUpdateDTO` fields and refactor exception handling with enhanced validation error responses

--- building-api-training/rest-api-training/ecommerce-api/src/main/java/com/gustavosantos/ecommerce_api/controllers/exceptions/GlobalExceptionHandler.java
+++ building-api-training/rest-api-training/ecommerce-api/src/main/java/com/gustavosantos/ecommerce_api/controllers/exceptions/GlobalExceptionHandler.java
@@ -1,18 +1,20 @@
-package com.gustavosantos.ecommerce_api.repositories.exceptions;
+package com.gustavosantos.ecommerce_api.controllers.exceptions;
 
 import com.gustavosantos.ecommerce_api.services.exceptions.DatabaseException;
 import com.gustavosantos.ecommerce_api.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.Instant;
 
 @ControllerAdvice
-public class ResourceExceptionHandler {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<StandardError> resourceNotFoundException(ResourceNotFoundException e, HttpServletRequest request) {
@@ -49,6 +51,32 @@ public class ResourceExceptionHandler {
                 HttpStatus.BAD_REQUEST.value(),
                 "Constraint violation",
                 e.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(standardError);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<StandardError> dataIntegrityViolation(DataIntegrityViolationException e, HttpServletRequest request) {
+
+        StandardError standardError = new StandardError(
+                Instant.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Data integrity violation",
+                e.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(standardError);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> methodArgumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request) {
+
+        StandardError standardError = new StandardError(
+                Instant.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Validation error",
+                e.getBindingResult().getAllErrors().get(0).getDefaultMessage(),
                 request.getRequestURI()
         );
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(standardError);
--- building-api-training/rest-api-training/ecommerce-api/src/main/java/com/gustavosantos/ecommerce_api/controllers/exceptions/StandardError.java
+++ building-api-training/rest-api-training/ecommerce-api/src/main/java/com/gustavosantos/ecommerce_api/controllers/exceptions/StandardError.java
@@ -1,4 +1,4 @@
-package com.gustavosantos.ecommerce_api.repositories.exceptions;
+package com.gustavosantos.ecommerce_api.controllers.exceptions;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
