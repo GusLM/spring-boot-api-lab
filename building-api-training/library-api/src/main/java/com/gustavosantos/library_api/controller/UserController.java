30206e00bcbc6c2adc01cd1177094746ce908602
Gustavo Santos <gustavolimarma@gmail.com>
2026-08-16 17:44:39 -0300

    Add `UserController` for handling user-related operations:
    - Implement `/users` endpoint with POST support to save users.

---
+++ building-api-training/library-api/src/main/java/com/gustavosantos/library_api/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.gustavosantos.library_api.controller;
+
+import com.gustavosantos.library_api.controller.dto.user.UserRequestDTO;
+import com.gustavosantos.library_api.model.User;
+import com.gustavosantos.library_api.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/users")
+public class UserController implements GenericController{
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<Void> save(@RequestBody @Valid UserRequestDTO dto) {
+        User user = userService.save(dto);
+        return ResponseEntity.created(headerLocationGenerator(user.getPublicId())).build();
+    }
+}
