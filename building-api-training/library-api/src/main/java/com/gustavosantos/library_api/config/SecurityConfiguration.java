d84b5eefe06aa656b1ec1036f0c220423c63e61e
Gustavo Santos <gustavolimarma@gmail.com>
2026-08-25 23:12:52 -0300

    Integrate custom user authentication:
    - Add `CustomUserDetailsService` to load user details for authentication.
    - Update `SecurityConfiguration` to integrate role-based authorization for `/books/**` and enable a custom login form.

--- building-api-training/library-api/src/main/java/com/gustavosantos/library_api/config/SecurityConfiguration.java
+++ building-api-training/library-api/src/main/java/com/gustavosantos/library_api/config/SecurityConfiguration.java
@@ -1,11 +1,15 @@
 package com.gustavosantos.library_api.config;
 
+import com.gustavosantos.library_api.model.UserRole;
+import com.gustavosantos.library_api.security.CustomUserDetailsService;
+import com.gustavosantos.library_api.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,13 +22,15 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                // Configuração de formulário de login customizado
+                // Configuração de formulário de “login” customizado
                 .formLogin(configurer -> {
-                    // Define a URL da página de login e permite acesso público a ela
+                    // Define a URL da página de “login” e permite acesso público a ela
                     configurer.loginPage("/login").permitAll();
                 })
                 .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers("/books/**").hasRole(String.valueOf(UserRole.ADMIN));
+
                     authorize.anyRequest().permitAll();
                 })
                 .build();
@@ -33,5 +39,10 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserService userService) {
+        return new CustomUserDetailsService(userService);
     }
 }
---
+++ building-api-training/library-api/src/main/java/com/gustavosantos/library_api/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.gustavosantos.library_api.security;
+
+import com.gustavosantos.library_api.model.User;
+import com.gustavosantos.library_api.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        User user = userService.findByLogin(login);
+
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(user.getLogin())
+                .password(user.getPassword())
+                .roles(String.valueOf(user.getRole()))
+                .build();
+    }
+}
