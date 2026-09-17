834925f7740d37ce17f9ca6ba17a1a71cf373152
Gustavo Santos <gustavolimarma@gmail.com>
2026-09-17 00:09:20 -0300

    feat(auth-server): implement CustomRegisteredClientRepository to integrate with ClientService

---
+++ building-api-training/library-api/src/main/java/com/gustavosantos/library_api/security/CustomRegisteredClientRepository.java
@@ -0,0 +1,55 @@
+package com.gustavosantos.library_api.security;
+
+import com.gustavosantos.library_api.service.ClientService;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomRegisteredClientRepository implements RegisteredClientRepository {
+
+    private final ClientService clientService;
+    private final TokenSettings tokenSettings;
+    private final ClientSettings clientSettings;
+
+    @Override
+    public void save(RegisteredClient registeredClient) {
+
+    }
+
+    @Override
+    public @Nullable RegisteredClient findById(String id) {
+        return null;
+    }
+
+    @Override
+    public @Nullable RegisteredClient findByClientId(String clientId) {
+        var client = clientService.findClientByClientId(clientId);
+
+        if (client == null) return null;
+
+        var builder =  RegisteredClient
+                .withId(client.getPublicId().toString())
+                .clientId(client.getClientId())
+                .clientSecret(client.getClientSecret())
+                .redirectUri(client.getRedirectUri())
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .tokenSettings(tokenSettings)
+                .clientSettings(clientSettings);
+
+        client.getScopes().forEach(builder::scope);
+
+        return builder.build();
+
+
+    }
+}
