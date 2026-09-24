020d1714ff7d9f881033363fc6912cf23147584f
Gustavo Santos <gustavolimarma@gmail.com>
2026-09-23 23:34:45 -0300

    feat(security): add OpenAPI docs support and configure Swagger access paths

--- building-api-training/library-api/src/main/java/com/gustavosantos/library_api/config/AuthorizationServerConfiguration.java
+++ building-api-training/library-api/src/main/java/com/gustavosantos/library_api/config/AuthorizationServerConfiguration.java
@@ -18,7 +18,6 @@ import org.springframework.security.config.annotation.web.configuration.OAuth2Au
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.core.OAuth2Token;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
