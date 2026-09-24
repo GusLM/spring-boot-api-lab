2a37f638b335c47d6a5b36f930045f26b90c9627
Gustavo Santos <gustavolimarma@gmail.com>
2026-09-24 00:25:05 -0300

    feat(config): add OpenAPI configuration for API documentation

---
+++ building-api-training/library-api/src/main/java/com/gustavosantos/library_api/config/OpenApiConfiguration.java
@@ -0,0 +1,20 @@
+package com.gustavosantos.library_api.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Library API",
+                version = "v1",
+                contact = @Contact(
+                        name = "Gustavo Santos",
+                        email = "gustavosantos@email.com"
+                )
+        )
+)
+public class OpenApiConfiguration {
+}
